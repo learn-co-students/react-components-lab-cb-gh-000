fa96ec064df229f89669cc2ee8643c4a20b12db4
N M <niclas@mettenleiter.com>
2019-08-16 15:58:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -32,8 +32,8 @@ export class InFrontOfYou extends Component {
   render() {
     return (
       <div className="infrontofyou">
-        <p>You shouldnt  look too far.</p>
-        <p>Sometimes, the solution is right in front of you.</p>
+        <p>"You shouldn't  look too far."</p>
+        <p>"Sometimes, the solution is right in front of you."</p>
       </div>
     )
   }
