f2198575e3c92d25790a2dd39cce78a43f15d494
Luisbert Seijas <luisbert2506@gmail.com>
2019-09-25 02:14:50 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,42 +1,51 @@
-// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
-
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
   'Short ribs',
   'Beef shin',
   'Ribeye'
-]
+];
 
-// Define these exported classes
-export class OlderCoaster extends Component {
-
-  // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  //
-}
-
-export class InFrontOfYou extends Component {
-  // your code here
-}
-
-export class ButcherShop extends Component {
-  // your code here
-}
-
-
-export class App extends Component {
+const OlderCoaster = React.createClass({
   render() {
-    return (
-      <div id="app">
+    return React.createElement('div', { className: 'oldercoaster' }, [
+      React.createElement('p', {}, 'Two grannies having the time of their life!'),
+      React.createElement('p', {}, 'Passengers:'),
+      React.createElement('ul', {}, [
+        React.createElement('li', {}, 'Agnes'),
+        React.createElement('li', {}, 'Muriel'),
+      ]),
+    ]);
+  }
+});
 
-      </div>
-    )
+class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement('div', {}, [
+      React.createElement('p', {}, "You shouldn't look too far."),
+      React.createElement('p', {}, [
+        'Sometimes, the solution is ',
+        React.createElement('strong', {}, 'right in front of you.')
+      ]),
+    ]);
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+class ButcherShop extends React.Component {
+  render() {
+    return React.createElement('div', { className: 'butcher-shop' }, [
+      React.createElement('p', {}, 'Hello! We have the following products for sale today:'),
+      React.createElement('ul', {},
+        BUTCHER_PRODUCTS.map(product => React.createElement('li', {}, product))
+      )
+    ]);
+  }
+}
+
+ReactDOM.render(
+  React.createElement('div', {}, [
+    React.createElement(OlderCoaster),
+    React.createElement(InFrontOfYou),
+    React.createElement(ButcherShop)
+  ]),
+  document.getElementById('main')
+);
