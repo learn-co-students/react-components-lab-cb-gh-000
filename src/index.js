736565aa8d56eb78481f8246efc9fc9b790cbcc4
ossama abd rahman <houssam.arghal1997@gmail.com>
2020-01-01 10:40:06 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import React, { Component } from 'react' // We need to import react so we can make use of its .component class
-// import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React, { Component } from 'react' // We need to import react so we can make use of its .component class
+import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -18,14 +18,47 @@ export class OlderCoaster extends Component {
   //   )
   // }
   //
+  render() {
+    return (
+      <div class="oldercoaster">
+        <p>Two grannies having the time of their life!</p>
+        <p>Passengers:</p>
+        <ul>
+          <li>Agnes</li>
+          <li>Muriel</li>
+        </ul>
+      </div>
+    )
+  }
 }
 
 export class InFrontOfYou extends Component {
   // your code here
+  render() {
+    return (
+      <div>
+        <p>{"You shouldn't look too far."}</p>
+        <p>Sometimes, the solution is right in front of you.</p>
+      </div>
+    )
+  }
 }
+
+let products = [<li>{BUTCHER_PRODUCTS[0]}</li>, <li>{BUTCHER_PRODUCTS[1]}</li>, <li>{BUTCHER_PRODUCTS[2]}</li>, <li>{BUTCHER_PRODUCTS[3]}</li>];
 
 export class ButcherShop extends Component {
   // your code here
+  render() {
+    return (
+      <div class="butcher-shop">
+        <p>Hello! We have the following products for sale today:</p>
+        <ul>
+          { products }
+        </ul>
+      </div>
+    )
+  }
+
 }
 
 
@@ -33,7 +66,9 @@ export class App extends Component {
   render() {
     return (
       <div id="app">
-
+          <OlderCoaster />
+          <InFrontOfYou />
+          <ButcherShop />
       </div>
     )
   }
